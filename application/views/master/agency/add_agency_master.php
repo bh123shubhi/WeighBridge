f2086989026dd388b85875d09c7ebae842d0dcaa
shubhibhandari <shubhi.bhandari@kreatetechnologies.com>
2020-06-28 16:23:52 +0530

    latest changes

--- application/views/master/agency/add_agency_master.php
+++ application/views/master/agency/add_agency_master.php
@@ -2,7 +2,7 @@
 <div class="page-header page-header-light">
     <div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
         <div class="d-flex">
-            <div class="breadcrumb"> <a href="index.html" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a> <span class="breadcrumb-item ">Agency Master List</span> <span class="breadcrumb-item active"> Add Agency Master </span> </div>
+            <div class="breadcrumb"> <a href="<?php echo base_url(); ?>dashboard" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a> <span class="breadcrumb-item ">Agency Master List</span> <span class="breadcrumb-item active"> Add Agency Master </span> </div>
             <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a> </div>
     </div>
 </div>
